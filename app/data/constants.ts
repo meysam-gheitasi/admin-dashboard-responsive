8d46f76991dec8a21a2840ed98335f840481a10f
meysam1920 <121888134+meysam1920@users.noreply.github.com>
2023-11-20 14:40:30 +0330

    Creating a constants.ts file in the data folder to display information such as menu data and such data

---
+++ app/data/constants.ts
@@ -0,0 +1,62 @@
+export const SIDEBAR_MENUS = [
+    {
+        title: "Pages",
+        list: [
+            {
+                title: "Dashboard",
+                path: "/dashboard",
+                icon: "<MdDashboard />",
+            },
+            {
+                title: "Users",
+                path: "/dashboard/users",
+                icon: "<MdSupervisedUserCircle />",
+            },
+            {
+                title: "Products",
+                path: "/dashboard/products",
+                icon: "<MdShoppingBag />",
+            },
+            {
+                title: "Transactions",
+                path: "/dashboard/transactions",
+                icon: "<MdAttachMoney />",
+            },
+        ],
+    },
+    {
+        title: "Analytics",
+        list: [
+            {
+                title: "Revenue",
+                path: "/dashboard/revenue",
+                icon: "<MdWork />",
+            },
+            {
+                title: "Reports",
+                path: "/dashboard/reports",
+                icon: "<MdAnalytics />",
+            },
+            {
+                title: "Teams",
+                path: "/dashboard/teams",
+                icon: "<MdPeople />",
+            },
+        ],
+    },
+    {
+        title: "User",
+        list: [
+            {
+                title: "Settings",
+                path: "/dashboard/settings",
+                icon: "<MdOutlineSettings />",
+            },
+            {
+                title: "Help",
+                path: "/dashboard/help",
+                icon: "<MdHelpCenter />",
+            },
+        ],
+    },
+];
