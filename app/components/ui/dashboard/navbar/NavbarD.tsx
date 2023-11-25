0f3ad03b84517e6943177256e29f88c3ad6fbed2
Meysam Gheitasi <121888134+meysam-gheitasi@users.noreply.github.com>
2023-11-25 21:54:38 +0330

    Adding the desired icons in the development of the NavbarD component and developing the styles of the title section so that the first letter is always displayed in large

--- app/components/ui/dashboard/navbar/NavbarD.tsx
+++ app/components/ui/dashboard/navbar/NavbarD.tsx
@@ -1,18 +1,34 @@
 "use client"
 
 import { usePathname } from "next/navigation"
+import {
+  MdNotifications,
+  MdOutlineChat,
+  MdPublic,
+  MdSearch,
+} from "react-icons/md";
 
 export default function NavbarD() {
 
   const pathname = usePathname()
 
   return (
-    <div className="flex justify-center flex-col w-full text-slate-200 bg-slate-800 p-5">
+    <div className="flex justify-center flex-col w-full text-slate-200 bg-slate-800 p-5 rounded-md">
       <div className="flex-between">
-        <h4>
+        <h4 className="capitalize font-semibold">
           {pathname.split('/').pop()}
         </h4>
-        <input />
+        <div className="flex-center gap-x-5">
+          <div className="flex-center gap-x-5">
+            <MdSearch />
+            <input className="p-1" type="text" placeholder="Search..." />
+          </div>
+          <div className="flex-start">
+            <MdOutlineChat size={20} />
+            <MdNotifications size={20} />
+            <MdPublic size={20} />
+          </div>
+        </div>
       </div>
     </div>
   )
